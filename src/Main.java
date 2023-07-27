397f7fca10e8b3eb2e74b2b590fa0fd6a18b24f8
wolfomania <kulyzhskyi.andrii@gmail.com>
2023-07-27 17:12:09 +0200

    random

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] arg) {
+    public static void main(String[] args) {
         MyGraphics g = new MyGraphics();
         MyLogic l = new MyLogic();
         g.setIntel(l);
